45f4a60badf01007274a0132b29e93afdd47f9a6
Existential Lamp <commumikuism@protonmail.com>
2022-12-01 23:00:51 -0900

    functioning code to parse the data and spit out maximum calorie count held by any single elf

--- day_01/day_01_A.cpp
+++ day_01/day_01_A.cpp
@@ -4,8 +4,40 @@
 * Advent of Code 2022
 * takes a list of ints, and finds the largest sequence
 */
+#include <iostream>
+#include <fstream>
+#include <vector>
 
-int main(){
-	return 0;
+using std::vector;
+using std::string;
+
+int main()
+{
+	std::ifstream calorie_list ("input.txt");
+	int input {0};
+	
+	//Vector storing how many calories each elf has
+	vector<int> total_calories;
+	int running_count {0};
+	while(calorie_list >> input)
+	{
+		running_count += input;
+		if(input == 0)
+		{
+			total_calories.push_back(running_count);
+			running_count = 0;
+		}
 	}
+	
+	int highest_count = 0;
+	for(int n : total_calories)
+	{
+		if(n > highest_count)
+		{
+			highest_count = n;
+		}
+	}
+	cout << highest_count;
+	return 0;
+}
 	
